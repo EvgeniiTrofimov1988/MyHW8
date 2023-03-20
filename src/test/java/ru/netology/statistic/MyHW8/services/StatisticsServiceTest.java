46fb558bd118c8a8f73c06b7ddcb3f288d9bb41d
повелитель <karakozova44d@mail.ru>
2023-03-20 13:26:10 +0300

    changed the folder location

--- src/test/java/ru/netology/statistic/MyHW8/services/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/MyHW8/services/StatisticsServiceTest.java
@@ -1,2 +1,43 @@
+package ru.netology.statistic.MyHW8.services;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class StatisticsServiceTest {
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxInTheEnd() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {4, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxIfNull() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {-8, -2, -6, -1, -15, -3, 0, 0, 0, 0};
+        long expected = 0;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
